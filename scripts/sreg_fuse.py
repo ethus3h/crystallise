9ab730496ef41347e28e5606194b05104de7b97e
ethus3h <kolubat@gmail.com>
2017-06-03 00:53:28 -0400

    Update Ember tree

--- scripts/sreg_fuse.py
+++ scripts/sreg_fuse.py
@@ -13,18 +13,6 @@ import tempfile
 
 from fuse import FUSE, FuseOSError, Operations, fuse_get_context
 
-def tracefunc(frame, event, arg, indent=[0]):
-    if event == "call":
-        indent[0] += 2
-        print "-" * indent[0] + "> call function", frame.f_code.co_name
-    elif event == "return":
-        print "<" + "-" * indent[0], "exit function", frame.f_code.co_name
-        indent[0] -= 2
-    return tracefunc
-
-import sys
-sys.settrace(tracefunc)
-
 class sreg_fuse(Operations):
     def __init__(self, root):
         self.root = root

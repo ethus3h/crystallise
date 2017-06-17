7548e6497109ec85cdc91a6c377b83b0d899656a
ethus3h <kolubat@gmail.com>
2017-06-17 10:26:00 -0400

    Update Ember tree

--- scripts/sreg_fuse.py
+++ scripts/sreg_fuse.py
@@ -36,13 +36,13 @@ class sreg_fuse(Operations):
         inputfile = io.open(source, 'r')
         outputfile = io.open(destination, 'w')
         subprocess.call(["sreg_read_stream"], stdin=inputfile, stdout=outputfile)
-        shutil.copy2(inputfile.name, outputfile.name)
+        shutil.copy2(source, outputfile.name)
 
     def _sreg_copy_write(self, source, destination):
         inputfile = io.open(source, 'r')
         outputfile = io.open(destination, 'w')
         subprocess.call(["sreg_store_stream"], stdin=inputfile, stdout=outputfile)
-        shutil.copy2(inputfile.name, outputfile.name)
+        shutil.copy2(source, outputfile.name)
 
     # Filesystem methods
     # ==================
